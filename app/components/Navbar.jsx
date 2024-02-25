318c403fe51c9725faf6530f1780350d1fb41043
Raj Vardhan Singh Birani <rajvardhan.singh@tmdc.io>
2024-02-25 19:18:26 +0530

    FEAT: added styling, navbar logo and some content to dashboard page

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "./dojo-logo.png";
 
 const Navbar = () => {
   return (
     <nav>
+      <Image
+        src={Logo}
+        alt="Biryani Helpdesk Logo"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
       <h1>Biryani Helpdesk</h1>
       <Link href={"/"}>Dashboard</Link>
       <Link href={"/tickets"}>Tickets</Link>
