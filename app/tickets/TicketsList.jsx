71a1a0c4cb05201bc3940fd58833e5227a6c4bf4
Raj Vardhan Singh Birani <rajvardhan.singh@tmdc.io>
2024-03-04 00:54:15 +0530

    FEAT: implemented loading comp and made resp changes, added delay in the fetch reqs of tickets and a ticket, to test loading

--- app/tickets/TicketsList.jsx
+++ app/tickets/TicketsList.jsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import React from "react";
 
 async function getTickets() {
+  //delay imitation
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   const res = await fetch(`http://localhost:4000/tickets`, {
     next: {
       revalidate: 0, //use 0 to opt out of using cache
